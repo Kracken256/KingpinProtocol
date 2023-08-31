c1346ac3b4119347e301d10cf11ed47a1ea19f5e
Wesley Jones <wesjones2004@gmail.com>
2023-08-30 17:23:40 -1000

    Primary header file

---
+++ include/kingpin.h
@@ -0,0 +1,16 @@
+#ifndef __KINGPIN_H_
+#define __KINGPIN_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+#include <types/kp.h>
+#include <kp/init.h>
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // __KINGPIN_H_
