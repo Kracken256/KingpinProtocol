373d3fdd1c3a30c3bdcc691deeac6a9b86e3f692
Wesley Jones <wesjones2004@gmail.com>
2023-08-30 17:19:58 -1000

    Added core types

---
+++ include/types/basic.h
@@ -0,0 +1,61 @@
+#ifndef __KINGPIN_TYPES_H_
+#define __KINGPIN_TYPES_H_
+
+#ifndef __KINGPIN_KP_H_
+#error "Do not include this file directly. Include types/kp.h instead."
+#endif // __KINGPIN_KP_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+    /// @brief Unsigned 8-bit integer
+    typedef unsigned char u8;
+
+    /// @brief Unsigned 16-bit integer
+    typedef unsigned short u16;
+
+    /// @brief Unsigned 32-bit integer
+    typedef unsigned int u32;
+
+    /// @brief Unsigned 64-bit integer
+    typedef unsigned long long u64;
+
+    /// @brief Signed 8-bit integer
+    typedef signed char s8;
+
+    /// @brief Signed 16-bit integer
+    typedef signed short s16;
+
+    /// @brief Signed 32-bit integer
+    typedef signed int s32;
+
+    /// @brief Signed 64-bit integer
+    typedef signed long long s64;
+
+    /// @brief 32-bit floating point number
+    typedef float f32;
+
+    /// @brief 64-bit floating point number
+    typedef double f64;
+
+    /// @brief 8-bit boolean
+    typedef u8 boolean;
+
+    /// @brief Standard size type
+    typedef u64 std_size_t;
+
+/// @brief Boolean true
+#define TRUE ((u8)1)
+
+/// @brief Boolean false
+#define FALSE ((u8)0)
+
+#define NULL ((void *)0)
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // __KINGPIN_TYPES_H_
---
+++ include/types/enum.h
@@ -0,0 +1,36 @@
+#ifndef __KINGPIN_ENUM_H_
+#define __KINGPIN_ENUM_H_
+
+#ifndef __KINGPIN_KP_H_
+#error "Do not include this file directly. Include types/kp.h instead."
+#endif // __KINGPIN_KP_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+    
+    /// @brief The status of a function call
+    /// @warning This status enum has success == 1 and fail == 0
+    typedef enum _kp_status
+    {
+        KP_OK = 1,
+        KP_FAIL = 0,
+        KP_DEPENDENCY_MISSING = 100,
+        KP_INVALID_ARGUMENT = 100,
+        KP_INVALID_STATE = 200,
+        KP_MEMORY_ERROR = 300,
+        KP_INTERNAL_ERROR = 400,
+        KP_NOT_IMPLEMENTED = 500,
+        KP_NOT_FOUND = 600,
+        KP_ALREADY_EXISTS = 700,
+        KP_OUT_OF_BOUNDS = 800,
+        KP_NOT_INITIALIZED = 900,
+        KP_NOT_SUPPORTED = 1000,
+    } kp_status;
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // __KINGPIN_ENUM_H_
