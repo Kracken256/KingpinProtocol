a57c284796b72838ccb5e4baddd7b7bb15c50d74
Wesley Jones <wesjones2004@gmail.com>
2023-08-30 21:09:57 -1000

    Added protocol structure definitions

---
+++ include/kp/proto.h
@@ -0,0 +1,42 @@
+#ifndef __KINGPIN_PROTO_H_
+#define __KINGPIN_PROTO_H_
+
+#ifndef __KINGPIN_H_
+#error "Do not include this file directly. Include kingpin.h instead."
+#endif // __KINGPIN_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+#include <types/kp.h>
+#include <types/x25519.h>
+
+    typedef struct _kp_connect_msg
+    {
+        u32 proclamation;
+        u8 sess_tp;
+        kp_x25519_public_key pubkey;
+        kp_ed25519_signature sig;
+    } kp_connect_msg;
+
+    /// @brief Initialize a Kingpin protocol connect message structure
+    /// @param msg The message structure to initialize
+    /// @param proclamation The proclamation to use
+    /// @param sess_tp The session type to use
+    /// @param pubkey The keypair to use
+    /// @return KP_SUCCESS on success, an error code otherwise
+    kp_status kp_init_connect_msg(kp_connect_msg *msg, u32 proclamation, u8 sess_tp, const kp_x25519_keypair *keypair);
+
+    /// @brief Synthesize a Kingpin protocol connect message, for transmission by user
+    /// @param buffer The buffer structure to use for the message
+    /// @param msg The connect message to synthesize
+    /// @return KP_SUCCESS on success, an error code otherwise
+    kp_status kp_syn_connect(kp_buffer *buffer, const kp_connect_msg *msg);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // __KINGPIN_PROTO_H_
---
+++ include/types/proto-structs.h
@@ -0,0 +1,82 @@
+#ifndef __KINGPIN_PROTO_STRUCTS_H_
+#define __KINGPIN_PROTO_STRUCTS_H_
+
+#ifndef __KINGPIN_BACKEND_H_
+#error "Do not include this file directly. Only backend .c files should do so."
+#endif // __KINGPIN_BACKEND_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+#include <types/kp.h>
+
+/// @brief The magic number for the Kingpin protocol init
+#define KP_PROTO_INIT_MAGIC 0x4B50
+
+/// @brief The magic number for the Kingpin protocol handshake response
+#define KP_PROTO_HANDSHAKE_RESP_MAGIC 0x4B52
+
+/// @brief The magic number for the Kingpin protocol ack
+#define KP_PROTO_ACK_MAGIC 0x4B51
+
+/// @brief The magic number for the Kingpin protocol data
+#define KP_PROTO_DAT_MAGIC 0x4B53
+
+/// @brief The magic number for the Kingpin protocol fin
+#define KP_PROTO_FIN_MAGIC 0x4B54
+
+/// @brief The Kingpin protocol version
+#define KP_PROTO_VERSION ((u16)0x01)
+
+#define KP_PROTO_SESSION_TYPE_GP 0x00
+
+#define KP_PROTO_FIN_FLAG_TERMINATE ((u16)0x02)
+
+#define KP_PROTO_ACK_FLAG_SUCCESS ((u16)(1 << 0))
+
+#define KP_PROTO_ACK_FLAG_FIN ((u16)(1 << 2))
+
+    typedef struct _kp_proto_handshake_init
+    {
+        u16 magic;
+        u16 version;
+        u32 proclamation;
+        u8 sess_tp;
+        u8 pubkey[32];
+        u8 sig[64];
+    } __attribute((packed)) kp_proto_handshake_init_sp;
+
+    typedef struct _kp_proto_ack
+    {
+        u16 magic;
+        u16 flags;
+        u32 crc32;
+    } __attribute((packed)) kp_proto_ack_sp;
+
+    typedef struct _kp_proto_handshake_resp
+    {
+        u16 magic;
+        u8 pubkey[32];
+        u8 sig[64];
+    } __attribute((packed)) kp_proto_handshake_resp_sp;
+
+    typedef struct _kp_proto_dat
+    {
+        u16 magic;
+        u8 flags;
+        u8 len[3]; // 24-bit length 16 MB max
+    } __attribute((packed)) kp_proto_dat_sp;
+
+    typedef struct _kp_proto_fin
+    {
+        u16 magic;
+        u16 flags;
+    } __attribute((packed)) kp_proto_fin_sp;
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // __KINGPIN_PROTO_STRUCTS_H_
