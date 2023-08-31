529dd52f65197b6ae6ab4029c0e91afcafcd6974
Wesley Jones <wesjones2004@gmail.com>
2023-08-30 17:23:31 -1000

    Public types header file

---
+++ include/types/kp.h
@@ -0,0 +1,18 @@
+#ifndef __KINGPIN_KP_H_
+#define __KINGPIN_KP_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+#include <types/basic.h>
+#include <types/enum.h>
+#include <types/struct.h>
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // __KINGPIN_KP_H_
