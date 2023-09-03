c4ffbebeb7a9c91785bee558a8308c48658f8540
Wesley Jones <wesjones2004@gmail.com>
2023-09-02 23:29:09 -1000

    Switched to correct ChaCha20 code

--- include/kp/cipher/chacha20.h
+++ include/kp/cipher/chacha20.h
@@ -1,30 +1,27 @@
-/* $OpenBSD: chacha.h,v 1.4 2016/08/27 04:04:56 guenther Exp $ */
-
-/*
-chacha-merged.c version 20080118
-D. J. Bernstein
-Public domain.
-*/
-
-#ifndef CHACHA_H
-#define CHACHA_H
+#pragma once
 
 #include <kp/types/kp.h>
 
-struct kp_chacha_ctx
+#ifdef __cplusplus 
+extern "C" {
+#endif
+
+struct chacha20_context
 {
-    u32 input[16];
+	u32 keystream32[16];
+	kp_size position;
+
+	u8 key[32];
+	u8 nonce[12];
+	u64 counter;
+
+	u32 state[16];
 };
 
-#define KP_CHACHA_MINKEYLEN 16
-#define KP_CHACHA_NONCELEN 8
-#define KP_CHACHA_CTRLEN 8
-#define KP_CHACHA_STATELEN (KP_CHACHA_NONCELEN + KP_CHACHA_CTRLEN)
-#define KP_CHACHA_BLOCKLEN 64
+void kp_chacha20_init_context(struct chacha20_context *ctx, u8 key[], u8 nounc[], u64 counter);
 
-void kp_chacha_keysetup(struct kp_chacha_ctx *x, const u8 *k, u32 kbits);
-void kp_chacha_ivsetup(struct kp_chacha_ctx *x, const u8 *iv, const u8 *ctr);
-void kp_chacha_encrypt_bytes(struct kp_chacha_ctx *x, const u8 *m, u8 *c,
-                          u32 bytes);
+void kp_chacha20_xor(struct chacha20_context *ctx, u8 *bytes, kp_size n_bytes);
 
-#endif /* CHACHA_H */
+#ifdef __cplusplus 
+}
+#endif 
--- source/kp/cipher/chacha20.c
+++ source/kp/cipher/chacha20.c
@@ -1,237 +1,133 @@
-/*
-chacha-merged.c version 20080118
-D. J. Bernstein
-Public domain.
-*/
-/// Modified from: https://github.com/jonasschnelli/chacha20poly1305
-
-
 #include <kp/cipher/chacha20.h>
 
-/* $OpenBSD: chacha.c,v 1.1 2013/11/21 00:45:44 djm Exp $ */
+#define __KINGPIN_BACKEND
+#include <kp/core.h>
 
-typedef struct chacha_ctx chacha_ctx;
-
-#define U8C(v) (v##U)
-#define U32C(v) (v##U)
-
-#define U8V(v) ((u8)(v) & U8C(0xFF))
-#define U32V(v) ((u32)(v) & U32C(0xFFFFFFFF))
-
-#define ROTL32(v, n) (U32V((v) << (n)) | ((v) >> (32 - (n))))
-
-#define U8TO32_LITTLE(p)                                              \
-    (((u32)((p)[0])) | ((u32)((p)[1]) << 8) | ((u32)((p)[2]) << 16) | \
-     ((u32)((p)[3]) << 24))
-
-#define U32TO8_LITTLE(p, v)      \
-    do                           \
-    {                            \
-        (p)[0] = U8V((v));       \
-        (p)[1] = U8V((v) >> 8);  \
-        (p)[2] = U8V((v) >> 16); \
-        (p)[3] = U8V((v) >> 24); \
-    } while (0)
-
-#define ROTATE(v, c) (ROTL32(v, c))
-#define XOR(v, w) ((v) ^ (w))
-#define PLUS(v, w) (U32V((v) + (w)))
-#define PLUSONE(v) (PLUS((v), 1))
-
-#define QUARTERROUND(a, b, c, d) \
-    a = PLUS(a, b);              \
-    d = ROTATE(XOR(d, a), 16);   \
-    c = PLUS(c, d);              \
-    b = ROTATE(XOR(b, c), 12);   \
-    a = PLUS(a, b);              \
-    d = ROTATE(XOR(d, a), 8);    \
-    c = PLUS(c, d);              \
-    b = ROTATE(XOR(b, c), 7);
-
-static const char sigma[16] = "expand 32-byte k";
-static const char tau[16] = "expand 16-byte k";
-
-void kp_chacha_keysetup(struct kp_chacha_ctx *x, const u8 *k, u32 kbits)
+static u32 rotl32(u32 x, int n)
 {
-    const s8 *constants;
-
-    x->input[4] = U8TO32_LITTLE(k + 0);
-    x->input[5] = U8TO32_LITTLE(k + 4);
-    x->input[6] = U8TO32_LITTLE(k + 8);
-    x->input[7] = U8TO32_LITTLE(k + 12);
-    if (kbits == 256)
-    { /* recommended */
-        k += 16;
-        constants = sigma;
-    }
-    else
-    { /* kbits == 128 */
-        constants = tau;
-    }
-    x->input[8] = U8TO32_LITTLE(k + 0);
-    x->input[9] = U8TO32_LITTLE(k + 4);
-    x->input[10] = U8TO32_LITTLE(k + 8);
-    x->input[11] = U8TO32_LITTLE(k + 12);
-    x->input[0] = U8TO32_LITTLE(constants + 0);
-    x->input[1] = U8TO32_LITTLE(constants + 4);
-    x->input[2] = U8TO32_LITTLE(constants + 8);
-    x->input[3] = U8TO32_LITTLE(constants + 12);
+    return (x << n) | (x >> (32 - n));
 }
 
-void kp_chacha_ivsetup(struct kp_chacha_ctx *x, const u8 *iv, const u8 *counter)
+static u32 pack4(const u8 *a)
 {
-    x->input[12] = counter == NULL ? 0 : U8TO32_LITTLE(counter + 0);
-    x->input[13] = counter == NULL ? 0 : U8TO32_LITTLE(counter + 4);
-    x->input[14] = U8TO32_LITTLE(iv + 0);
-    x->input[15] = U8TO32_LITTLE(iv + 4);
+    u32 res = 0;
+    res |= (u32)a[0] << 0 * 8;
+    res |= (u32)a[1] << 1 * 8;
+    res |= (u32)a[2] << 2 * 8;
+    res |= (u32)a[3] << 3 * 8;
+    return res;
 }
 
-void kp_chacha_encrypt_bytes(struct kp_chacha_ctx *x, const u8 *m, u8 *c, u32 bytes)
+static void unpack4(u32 src, u8 *dst)
 {
-    u32 x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15;
-    u32 j0, j1, j2, j3, j4, j5, j6, j7, j8, j9, j10, j11, j12, j13, j14, j15;
-    u8 *ctarget = NULL;
-    u8 tmp[64];
-    u32 i;
+    dst[0] = (src >> 0 * 8) & 0xff;
+    dst[1] = (src >> 1 * 8) & 0xff;
+    dst[2] = (src >> 2 * 8) & 0xff;
+    dst[3] = (src >> 3 * 8) & 0xff;
+}
 
-    if (!bytes)
-        return;
+static void chacha20_init_block(struct chacha20_context *ctx, u8 key[], u8 nonce[])
+{
+    kp_memcpy(ctx->key, key, sizeof(ctx->key));
+    kp_memcpy(ctx->nonce, nonce, sizeof(ctx->nonce));
 
-    j0 = x->input[0];
-    j1 = x->input[1];
-    j2 = x->input[2];
-    j3 = x->input[3];
-    j4 = x->input[4];
-    j5 = x->input[5];
-    j6 = x->input[6];
-    j7 = x->input[7];
-    j8 = x->input[8];
-    j9 = x->input[9];
-    j10 = x->input[10];
-    j11 = x->input[11];
-    j12 = x->input[12];
-    j13 = x->input[13];
-    j14 = x->input[14];
-    j15 = x->input[15];
+    const u8 *magic_constant = (u8 *)"expand 32-byte k";
+    ctx->state[0] = pack4(magic_constant + 0 * 4);
+    ctx->state[1] = pack4(magic_constant + 1 * 4);
+    ctx->state[2] = pack4(magic_constant + 2 * 4);
+    ctx->state[3] = pack4(magic_constant + 3 * 4);
+    ctx->state[4] = pack4(key + 0 * 4);
+    ctx->state[5] = pack4(key + 1 * 4);
+    ctx->state[6] = pack4(key + 2 * 4);
+    ctx->state[7] = pack4(key + 3 * 4);
+    ctx->state[8] = pack4(key + 4 * 4);
+    ctx->state[9] = pack4(key + 5 * 4);
+    ctx->state[10] = pack4(key + 6 * 4);
+    ctx->state[11] = pack4(key + 7 * 4);
+    // 64 bit counter initialized to zero by default.
+    ctx->state[12] = 0;
+    ctx->state[13] = pack4(nonce + 0 * 4);
+    ctx->state[14] = pack4(nonce + 1 * 4);
+    ctx->state[15] = pack4(nonce + 2 * 4);
 
-    for (;;)
+    kp_memcpy(ctx->nonce, nonce, sizeof(ctx->nonce));
+}
+
+static void chacha20_block_set_counter(struct chacha20_context *ctx, u64 counter)
+{
+    ctx->state[12] = (u32)counter;
+    ctx->state[13] = pack4(ctx->nonce + 0 * 4) + (u32)(counter >> 32);
+}
+
+static void chacha20_block_next(struct chacha20_context *ctx)
+{
+    // This is where the crazy voodoo magic happens.
+    // Mix the bytes a lot and hope that nobody finds out how to undo it.
+    for (int i = 0; i < 16; i++)
+        ctx->keystream32[i] = ctx->state[i];
+
+#define CHACHA20_QUARTERROUND(x, a, b, c, d) \
+    x[a] += x[b];                            \
+    x[d] = rotl32(x[d] ^ x[a], 16);          \
+    x[c] += x[d];                            \
+    x[b] = rotl32(x[b] ^ x[c], 12);          \
+    x[a] += x[b];                            \
+    x[d] = rotl32(x[d] ^ x[a], 8);           \
+    x[c] += x[d];                            \
+    x[b] = rotl32(x[b] ^ x[c], 7);
+
+    for (int i = 0; i < 10; i++)
     {
-        if (bytes < 64)
-        {
-            if (m != NULL)
-            {
-                for (i = 0; i < bytes; ++i)
-                {
-                    tmp[i] = m[i];
-                }
-                m = tmp;
-            }
-            ctarget = c;
-            c = tmp;
-        }
-        x0 = j0;
-        x1 = j1;
-        x2 = j2;
-        x3 = j3;
-        x4 = j4;
-        x5 = j5;
-        x6 = j6;
-        x7 = j7;
-        x8 = j8;
-        x9 = j9;
-        x10 = j10;
-        x11 = j11;
-        x12 = j12;
-        x13 = j13;
-        x14 = j14;
-        x15 = j15;
-        for (i = 20; i > 0; i -= 2)
-        {
-            QUARTERROUND(x0, x4, x8, x12)
-            QUARTERROUND(x1, x5, x9, x13)
-            QUARTERROUND(x2, x6, x10, x14)
-            QUARTERROUND(x3, x7, x11, x15)
-            QUARTERROUND(x0, x5, x10, x15)
-            QUARTERROUND(x1, x6, x11, x12)
-            QUARTERROUND(x2, x7, x8, x13)
-            QUARTERROUND(x3, x4, x9, x14)
-        }
-        x0 = PLUS(x0, j0);
-        x1 = PLUS(x1, j1);
-        x2 = PLUS(x2, j2);
-        x3 = PLUS(x3, j3);
-        x4 = PLUS(x4, j4);
-        x5 = PLUS(x5, j5);
-        x6 = PLUS(x6, j6);
-        x7 = PLUS(x7, j7);
-        x8 = PLUS(x8, j8);
-        x9 = PLUS(x9, j9);
-        x10 = PLUS(x10, j10);
-        x11 = PLUS(x11, j11);
-        x12 = PLUS(x12, j12);
-        x13 = PLUS(x13, j13);
-        x14 = PLUS(x14, j14);
-        x15 = PLUS(x15, j15);
+        CHACHA20_QUARTERROUND(ctx->keystream32, 0, 4, 8, 12)
+        CHACHA20_QUARTERROUND(ctx->keystream32, 1, 5, 9, 13)
+        CHACHA20_QUARTERROUND(ctx->keystream32, 2, 6, 10, 14)
+        CHACHA20_QUARTERROUND(ctx->keystream32, 3, 7, 11, 15)
+        CHACHA20_QUARTERROUND(ctx->keystream32, 0, 5, 10, 15)
+        CHACHA20_QUARTERROUND(ctx->keystream32, 1, 6, 11, 12)
+        CHACHA20_QUARTERROUND(ctx->keystream32, 2, 7, 8, 13)
+        CHACHA20_QUARTERROUND(ctx->keystream32, 3, 4, 9, 14)
+    }
 
-        if (m != NULL)
-        {
-            x0 = XOR(x0, U8TO32_LITTLE(m + 0));
-            x1 = XOR(x1, U8TO32_LITTLE(m + 4));
-            x2 = XOR(x2, U8TO32_LITTLE(m + 8));
-            x3 = XOR(x3, U8TO32_LITTLE(m + 12));
-            x4 = XOR(x4, U8TO32_LITTLE(m + 16));
-            x5 = XOR(x5, U8TO32_LITTLE(m + 20));
-            x6 = XOR(x6, U8TO32_LITTLE(m + 24));
-            x7 = XOR(x7, U8TO32_LITTLE(m + 28));
-            x8 = XOR(x8, U8TO32_LITTLE(m + 32));
-            x9 = XOR(x9, U8TO32_LITTLE(m + 36));
-            x10 = XOR(x10, U8TO32_LITTLE(m + 40));
-            x11 = XOR(x11, U8TO32_LITTLE(m + 44));
-            x12 = XOR(x12, U8TO32_LITTLE(m + 48));
-            x13 = XOR(x13, U8TO32_LITTLE(m + 52));
-            x14 = XOR(x14, U8TO32_LITTLE(m + 56));
-            x15 = XOR(x15, U8TO32_LITTLE(m + 60));
-        }
-        j12 = PLUSONE(j12);
-        if (!j12)
-        {
-            j13 = PLUSONE(j13);
-            /* stopping at 2^70 bytes per nonce is user's responsibility */
-        }
+    for (int i = 0; i < 16; i++)
+        ctx->keystream32[i] += ctx->state[i];
 
-        U32TO8_LITTLE(c + 0, x0);
-        U32TO8_LITTLE(c + 4, x1);
-        U32TO8_LITTLE(c + 8, x2);
-        U32TO8_LITTLE(c + 12, x3);
-        U32TO8_LITTLE(c + 16, x4);
-        U32TO8_LITTLE(c + 20, x5);
-        U32TO8_LITTLE(c + 24, x6);
-        U32TO8_LITTLE(c + 28, x7);
-        U32TO8_LITTLE(c + 32, x8);
-        U32TO8_LITTLE(c + 36, x9);
-        U32TO8_LITTLE(c + 40, x10);
-        U32TO8_LITTLE(c + 44, x11);
-        U32TO8_LITTLE(c + 48, x12);
-        U32TO8_LITTLE(c + 52, x13);
-        U32TO8_LITTLE(c + 56, x14);
-        U32TO8_LITTLE(c + 60, x15);
+    u32 *counter = ctx->state + 12;
+    // increment counter
+    counter[0]++;
+    if (0 == counter[0])
+    {
+        // wrap around occured, increment higher 32 bits of counter
+        counter[1]++;
+        // Limited to 2^64 blocks of 64 bytes each.
+        // If you want to process more than 1180591620717411303424 bytes
+        // you have other problems.
+        // We could keep counting with counter[2] and counter[3] (nonce),
+        // but then we risk reusing the nonce which is very bad.
+    }
+}
 
-        if (bytes <= 64)
+void kp_chacha20_init_context(struct chacha20_context *ctx, u8 key[], u8 nonce[], u64 counter)
+{
+    kp_memset(ctx, 0, sizeof(struct chacha20_context));
+
+    chacha20_init_block(ctx, key, nonce);
+    chacha20_block_set_counter(ctx, counter);
+
+    ctx->counter = counter;
+    ctx->position = 64;
+}
+
+void kp_chacha20_xor(struct chacha20_context *ctx, u8 *bytes, kp_size n_bytes)
+{
+    u8 *keystream8 = (u8 *)ctx->keystream32;
+    for (kp_size i = 0; i < n_bytes; i++)
+    {
+        if (ctx->position >= 64)
         {
-            if (bytes < 64)
-            {
-                for (i = 0; i < bytes; ++i)
-                    ctarget[i] = c[i];
-            }
-            x->input[12] = j12;
-            x->input[13] = j13;
-            return;
+            chacha20_block_next(ctx);
+            ctx->position = 0;
         }
-        bytes -= 64;
-        c += 64;
-        if (m != NULL)
-        {
-            m += 64;
-        }
+        bytes[i] ^= keystream8[ctx->position];
+        ctx->position++;
     }
 }
